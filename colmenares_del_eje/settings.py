21f33c8e91e934e559d2d1abb458ee90a86b7002
DanielHerreraFD <danieldelgadofh@gmail.com>
2025-03-26 22:06:35 -0500

    cambiando urls al nuevo dominio del frontend desplegado

--- colmenares_del_eje/settings.py
+++ colmenares_del_eje/settings.py
@@ -208,9 +208,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://sub.example.com",
-    "http://localhost:5500",
-    "http://localhost:5173"
+    'https://deploy-colmenares.vercel.app/'
 ]
 
 

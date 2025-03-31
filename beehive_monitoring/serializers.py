aba083da03e4036c22e2b1e95d131b3eafd7d53f
DanielHerreraFD <danieldelgadofh@gmail.com>
2025-03-31 10:57:06 -0500

    mandando estado de la colmena en el serializador de monitoreo y recoleccion

--- beehive_monitoring/serializers.py
+++ beehive_monitoring/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers  
 from beehive_monitoring.models import Monitoring
 from users.models import CustomUser
+from hive_management.models import Beehive
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name']
 
+class BeehiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Beehive
+        fields = ['id', 'status']
 
 class MonitoringSerializerInput(serializers.ModelSerializer):
     class Meta:
@@ -22,6 +27,7 @@ class MonitoringSerializerInput(serializers.ModelSerializer):
 
 class MonitoringSerializerOutput(serializers.ModelSerializer):
     beekeeper = CustomUserSerializer()
+    hive_id = BeehiveSerializer()
 
     class Meta:
         model = Monitoring
--- hive_harvesting/serializers.py
+++ hive_harvesting/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from hive_harvesting.models import harvesting
 from users.models import CustomUser
+from hive_management.models import Beehive
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name']
 
+class BeehiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Beehive
+        fields = ['id', 'status']
 
 class InputHarvestingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,6 +26,7 @@ class InputHarvestingSerializer(serializers.ModelSerializer):
 
 class OutputHarvestingSerializer(serializers.ModelSerializer):
     beekeeper = CustomUserSerializer()
+    hive_id = BeehiveSerializer()
 
     class Meta:
         model = harvesting
